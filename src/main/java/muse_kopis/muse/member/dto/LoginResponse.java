fbdbb6389cb9d32a76fc11efbf882b19298b802a
jtpark <jtpark@gaonpf.com>
2024-11-22 11:20:38 +0900

    refactor : 사용하지 않는 자체 회원관리 기능 제거

--- src/main/java/muse_kopis/muse/member/dto/LoginResponse.java
+++
@@ -1,7 +0,0 @@
-package muse_kopis.muse.member.dto;
-
-public record LoginResponse(
-        String token,
-        Boolean isNewUser
-) {
-}
--- src/main/java/muse_kopis/muse/member/dto/SignUpRequest.java
+++
@@ -1,8 +0,0 @@
-package muse_kopis.muse.member.dto;
-
-public record SignUpRequest(
-        String username,
-        String password,
-        String name
-) {
-}
