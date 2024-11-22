ff0445371c368ac81cdce557cffc60652bd537d1
jtpark <jtpark@gaonpf.com>
2024-11-22 13:46:10 +0900

    feat: 관심 배우 지정 조회 기능 추가

--- src/main/java/muse_kopis/muse/performance/actor/ActorService.java
+++ src/main/java/muse_kopis/muse/performance/actor/ActorService.java
@@ -3,15 +3,19 @@ package muse_kopis.muse.performance.actor;
 import lombok.RequiredArgsConstructor;
 import muse_kopis.muse.auth.oauth.domain.OauthMember;
 import muse_kopis.muse.auth.oauth.domain.OauthMemberRepository;
-import muse_kopis.muse.performance.castmember.CastMember;
-import muse_kopis.muse.performance.castmember.CastMemberRepository;
+import muse_kopis.muse.performance.actor.dto.ActorDto;
+import muse_kopis.muse.performance.actor.domain.FavoriteActor;
+import muse_kopis.muse.performance.actor.repository.FavoriteActorRepository;
 import muse_kopis.muse.performance.usergenre.UserGenre;
 import muse_kopis.muse.performance.usergenre.UserGenreRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-public class FavoriteActorService {
+public class ActorService {
 
     private final FavoriteActorRepository favoriteActorRepository;
     private final OauthMemberRepository oauthMemberRepository;
@@ -20,8 +24,14 @@ public class FavoriteActorService {
     public Long favorite(Long memberId, String actorsName) {
         OauthMember oauthMember = oauthMemberRepository.getByOauthMemberId(memberId);
         UserGenre userGenre = userGenreRepository.getUserGenreByOauthMember(oauthMember);
-        FavoriteActor favoriteActor = new FavoriteActor(userGenre, actorsName);
+        FavoriteActor favoriteActor = new FavoriteActor(memberId, actorsName, userGenre);
         FavoriteActor save = favoriteActorRepository.save(favoriteActor);
         return save.id();
+    }
+
+    public List<ActorDto> favorites(Long memberId) {
+        return favoriteActorRepository.findAllByMemberId(memberId).stream()
+                .map(ActorDto::from)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/muse_kopis/muse/performance/actor/repository/FavoriteActorRepository.java
+++ src/main/java/muse_kopis/muse/performance/actor/repository/FavoriteActorRepository.java
@@ -1,8 +1,13 @@
-package muse_kopis.muse.performance.actor;
+package muse_kopis.muse.performance.actor.repository;
 
+import muse_kopis.muse.performance.actor.domain.FavoriteActor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FavoriteActorRepository extends JpaRepository<FavoriteActor, Long> {
+
+    List<FavoriteActor> findAllByMemberId(Long memberId);
 }
